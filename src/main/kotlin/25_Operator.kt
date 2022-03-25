077bcd21ba7099549bf0355b6c3ceba446ce6401
Joki <jokisaputrae30@gmail.com>
2022-03-25 08:08:27 +0700

    when

---
+++ src/main/kotlin/25_Operator.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var maybeNumber: Int? = 15
+    maybeNumber = null
+    var s1 = "Hello"
+    var s2: String? = "World"
+    println(s1.length)
+    if (s2 != null) {
+        println(s2.length)
+    }
+}
